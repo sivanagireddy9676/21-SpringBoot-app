01ad1e2e1c5c1f5fc23e81c12f79e23aadaa25e2
sivanagireddy9676 <seetarusivanagireddy@gmail.com>
2024-06-16 10:16:25 +0530

    HIS-300:Related changes

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -8,6 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Application {
 
 	public static void main(String[] args) {
+		private static final String REST_URL="https://www.equifax.com/getScores";
 		//SpringApplication.run(Application.class, args); 
 		
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
