5ffbe5280b50118aeb93b980937f3fe3e1e08c1d
sivanagireddy9676 <seetarusivanagireddy@gmail.com>
2024-06-16 10:59:53 +0530

    HIS-301:Related changes

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -6,14 +6,18 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
+	private Map<String,object> cache = new HashMap<String,object>()
 
 	public static void main(String[] args) {
 		//SpringApplication.run(Application.class, args); 
-		
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-		//changes for HIS-134
+		//change  s for HIS-134
 		int i=10;
 		run.close();
+	}
+	//HIS-301 changes   
+	public void loadDataToCache(){
+		//logie
 	}
 
 }
